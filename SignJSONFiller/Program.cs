bb2a5de0b45abe6f6afcb96c11a552419d8de78a
Hugo Bachér <huba02001@utb.vaxjo.se>
2020-04-16 10:54:41 +0200

    Load function to load an old file

--- SignJSONFiller/Program.cs
+++ SignJSONFiller/Program.cs
@@ -5,113 +5,142 @@ using Newtonsoft.Json;
 
 namespace SignJSONFiller
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            App app = new App();
-            app.Start();
-        }
-        class App
-        {
-            List<Sign> signList = new List<Sign>();
-            public void Start()
-            {
-                start:
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write($"Start time: {DateTime.Now}");
+			App app = new App();
+			app.Start();
+		}
+		class App
+		{
+			string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			string fileName = "Data.json";
+			List<Sign> signList = new List<Sign>();
+			public void Start()
+			{
+				start:
 
-                Console.Write("\nAction (Add/Write/Exit/Error): ");
-                string answer = Console.ReadLine().ToLower();
+				Console.Write("\nAction (Add/Write/Load/Exit/Error/Help): ");
+				string answer = Console.ReadLine().ToLower();
 
-                if (answer == "exit" || answer == "e" || answer == "esc")
-                {
-                    Console.Write("Are you sure you want to exit? (Yes/No): ");
-                    answer = Console.ReadLine().ToLower();
-                    if (answer == "yes" || answer == "y")
-                        Environment.Exit(1);
-                    else
-                        goto start;
-                }
-                else if (answer == "write" || answer == "w" || answer == "print" || answer == "p")
-                {
-                    Console.Write("Are you sure you want to write (this will overwrite the existing file)? (Yes/No): ");
-                    answer = Console.ReadLine().ToLower();
-                    if (answer == "yes" || answer == "y")
-                        Write();
-                    else
-                        goto start;
+				if (answer == "exit" || answer == "e" || answer == "esc" || answer == "end")
+				{
+					Console.Write("Are you sure you want to exit? (Yes/No): ");
+					answer = Console.ReadLine().ToLower();
+					if (answer == "yes" || answer == "y")
+						Environment.Exit(1);
+					else
+						goto start;
+				}
+				else if (answer == "write" || answer == "w" || answer == "print" || answer == "p")
+				{
+					Console.Write("Are you sure you want to write (this will overwrite the existing file)? (Yes/No): ");
+					answer = Console.ReadLine().ToLower();
+					if (answer == "yes" || answer == "y")
+						Write();
+					else
+						goto start;
 
-                    Console.Write("Do you want to exit? (Yes/No): ");
-                    answer = Console.ReadLine().ToLower();
-                    if (answer == "yes" || answer == "y")
-                        Environment.Exit(2);
-                    else
-                        goto start;
-                }
-                else if (answer == "error" || answer == "err")
-                {
-                    Console.WriteLine("Error codes:");
-                    Console.WriteLine("0: Normal program end");
-                    Console.WriteLine("1: Exit command");
-                    Console.WriteLine("2: Conent written to file & ended");
-                }
-                else
-                {
-                    Add();
-                    goto start;
-                }
-                Console.Write(JsonConvert.SerializeObject(signList));
-            }
+					Console.Write("Do you want to exit? (Yes/No): ");
+					answer = Console.ReadLine().ToLower();
+					if (answer == "yes" || answer == "y")
+						Environment.Exit(2);
+					else
+						goto start;
+				}
+				else if(answer == "load" || answer == "l")
+				{
+					Console.Write("Are you sure you want to load file? (Yes/No): ");
+					answer = Console.ReadLine().ToLower();
+					if (answer == "yes" || answer == "y")
+						Read();
+					else
+						goto start;
+				}
+				else if (answer == "error" || answer == "err")
+				{
+					Console.WriteLine("=== Exit codes ===");
+					Console.WriteLine("0: Normal program end");
+					Console.WriteLine("1: Exit command");
+					Console.WriteLine("2: Conent written to file & ended");
 
-            public void Add()
-            {
-                Console.Write("ID: ");
-                string id = Console.ReadLine().ToUpper();
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Image: ");
-                string image = Console.ReadLine().ToLower();
-                Console.Write("Description: ");
-                string description = Console.ReadLine();
-                Console.Write("Category (Split with ,): ");
-                List<string> categories = new List<string>(Console.ReadLine().Split(','));
-                Sign sign = new Sign(id, name, image, description, categories);
-                signList.Add(sign);
-            }
-            
-            public void Write()
-            {
-                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                string fileName = "sign.json";
+					goto start;
+				}
+				else if (answer == "help" || answer == "h")
+				{
+					Console.WriteLine("=== Help ===");
+					Console.WriteLine("Add: add more signs (Alias: enter)");
+					Console.WriteLine("Write: write content to file (Alias: w, print, p)");
+					Console.WriteLine("Load: load content from file (Alias: l)");
+					Console.WriteLine("Exit: stop the program (NOTE: file wont be saved) (Alias: e,esc,end)");
+					Console.WriteLine("Error: exit codes (Alias: err)");
+					Console.WriteLine("Help: this command (Alias: h)");
 
-                File.WriteAllText(Path.Combine(filePath, fileName), JsonConvert.SerializeObject(signList));
+					goto start;
+				}
+				else
+				{
+					Add();
+					goto start;
+				}
+				Console.Write(JsonConvert.SerializeObject(signList));
+			}
 
-                Console.WriteLine($"File saved as {fileName} in your Documents folder");
-            }
-        }
+			public void Add()
+			{
+				Console.Write("ID: ");
+				string id = Console.ReadLine().ToUpper();
+				Console.Write("Name: ");
+				string name = Console.ReadLine();
+				Console.Write("Image: ");
+				string image = Console.ReadLine().ToLower();
+				Console.Write("Description: ");
+				string description = Console.ReadLine();
+				Console.Write("Category (Split with ,): ");
+				List<string> categories = new List<string>(Console.ReadLine().Split(','));
+				Sign sign = new Sign(id, name, image, description, categories);
+				signList.Add(sign);
+			}
+			
+			public void Write()
+			{
+				File.WriteAllText(Path.Combine(filePath, fileName), JsonConvert.SerializeObject(signList));
 
-        [System.Serializable]
-        public class Sign
-        {
-            public string id;
-            public string name;
-            public string imageName;
-            public string description;
-            public List<string> categories = new List<string>();
+				Console.WriteLine($"File saved as {fileName} in your Documents folder");
+			}
 
-            public Sign(string id, string name, string imageName, string description, List<string> categories)
-            {
-                this.id = id;
-                this.name = name;
-                this.imageName = imageName;
-                this.description = description;
-                this.categories = categories;
-            }
+			public void Read()
+			{
+				List<Sign> tempList = JsonConvert.DeserializeObject<List<Sign>>(File.ReadAllText(Path.Combine(filePath, fileName)));
+				signList.AddRange(tempList);
+			}
+		}
 
-            public Sign(string id, string name)
-            {
-                this.id = id;
-                this.name = name;
-            }
-        }
-    }
+		[System.Serializable]
+		public class Sign
+		{
+			public string id;
+			public string name;
+			public string imageName;
+			public string description;
+			public List<string> categories = new List<string>();
+
+			public Sign(string id, string name, string imageName, string description, List<string> categories)
+			{
+				this.id = id;
+				this.name = name;
+				this.imageName = imageName;
+				this.description = description;
+				this.categories = categories;
+			}
+
+			public Sign(string id, string name)
+			{
+				this.id = id;
+				this.name = name;
+			}
+		}
+	}
 }
